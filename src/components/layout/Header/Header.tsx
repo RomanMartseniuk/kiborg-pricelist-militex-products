428bd411fc5b44eb20cf5749bc79c70f485b9f85
Roman Martseniuk <martseniuk.roma@gmail.com>
2026-02-20 22:16:04 +0200

    complete Header styles and fix URL for visiting shop

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -13,29 +13,30 @@ import ViberIcon from "@/assets/icons/ViberIcon";
 export const Header = () => {
   return (
     <>
-      <header className="py-2 pt-12.5 lg:pt-25 flex flex-col sm:flex-row sm:items-center sm:justify-between md:justify-center md:gap-15 mb-12.5 lg:mb-18">
+      <header className="py-2 pt-12.5 lg:pt-25 flex flex-col md:flex-row md:items-center sm:justify-center md:gap-15 mb-12.5 lg:mb-18">
         <h1 className="flex flex-col items-center gap-3.5 mb-4 lg:flex-row">
-          <p className="text-[64px] font-semibold font-[Unbounded] text-[#6E6E49] uppercase">
+          <p className="text-[64px] font-semibold font-[Unbounded] text-[#202020] uppercase">
             ПРАЙС
           </p>
-          <img className="h-16" src={logo} alt="KIBORG" />
+          <img className="h-30" src={logo} alt="KIBORG" />
         </h1>
 
         <div className="flex flex-col items-center gap-5">
           <a
-            href="https://kiborg.com.ua/"
-            className="block font-[Unbounded] uppercase font-[16px] bg-[#434343] text-white rounded-[15px] w-75 py-3.75 text-center transition-all hover:-translate-y-1 hover:bg-[#525252]"
+            target="_blank"
+            href="https://militex.in.ua/"
+            className="block font-[Unbounded] uppercase font-[16px] bg-[#202020] text-[#fecb15] rounded-[15px] w-75 py-3.75 text-center transition-all duration-500 hover:-translate-y-1 hover:bg-[#fecb15] hover:text-[#202020]"
           >
             ПЕРЕЙТИ НА САЙТ
           </a>
           <Dialog>
             <DialogTrigger asChild>
-              <div className="block font-[Unbounded] uppercase font-[16px] bg-[#8F9250] text-white rounded-[15px] w-75 py-3.75 text-center cursor-pointer transition-all hover:-translate-y-1 hover:bg-[#6E6E49]">
+              <div className="block font-[Unbounded] uppercase font-[16px] bg-[#fecb15] text-[#202020] rounded-[15px] w-75 py-3.75 text-center cursor-pointer transition-all duration-500 hover:-translate-y-1 hover:bg-[#202020] hover:text-[#fecb15]">
                 ЗАМОВЛЕННЯ ТА ПИТАННЯ
               </div>
             </DialogTrigger>
             <DialogContent className="">
-              <DialogTitle className="text-center font-[Unbounded] text-[#6E6E49]">
+              <DialogTitle className="text-center font-[Unbounded] text-[#202020]">
                 ЗАМОВЛЕННЯ ТА ПИТАННЯ
               </DialogTitle>
               <hr className="border-black opacity-80" />
@@ -55,18 +56,18 @@ export const Header = () => {
                     />
                     <div>
                       <p className="opacity-90 text-[16px]">Менеджер</p>
-                      <p className="text-[24px]">Кіборг</p>
+                      <p className="text-[24px]">Militex</p>
                     </div>
                   </div>
                   <a
-                    className="flex items-center gap-1 transition-colors hover:text-[#8F9250] text-[14px]"
+                    className="flex items-center gap-1 transition-colors hover:text-[#fecb15] text-[14px]"
                     href="https://t.me/@Kiborg_drop_manager"
                   >
                     <TelegramIcon className="size-6" />
                     <p>@Kiborg_drop_manager</p>
                   </a>
                   <a
-                    className="flex items-center gap-1 transition-colors hover:text-[#8F9250] text-[14px]"
+                    className="flex items-center gap-1 transition-colors hover:text-[#fecb15] text-[14px]"
                     href="viber://chat?number=%2B380952055505"
                   >
                     <ViberIcon className="size-6" />
@@ -82,18 +83,18 @@ export const Header = () => {
                     />
                     <div>
                       <p className="opacity-90 text-[16px]">Менеджер</p>
-                      <p className="text-[24px]">Кіборг</p>
+                      <p className="text-[24px]">Militex</p>
                     </div>
                   </div>
                   <a
-                    className="flex items-center gap-1 transition-colors hover:text-[#8F9250] text-[14px]"
+                    className="flex items-center gap-1 transition-colors hover:text-[#fecb15] text-[14px]"
                     href="https://t.me/@Kiborg_opt"
                   >
                     <TelegramIcon className="size-6" />
                     <p>@Kiborg_opt</p>
                   </a>
                   <a
-                    className="flex items-center gap-1 transition-colors hover:text-[#8F9250] text-[14px]"
+                    className="flex items-center gap-1 transition-colors hover:text-[#fecb15] text-[14px]"
                     href="viber://chat?number=%2B380955077707"
                   >
                     <ViberIcon className="size-6" />
